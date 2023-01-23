b74443f5d6dc20123561bfb5c383f4c0c2dab415
Jovan253 <54801590+Jovan253@users.noreply.github.com>
2023-01-23 20:22:25 +0000

    react netflix cone

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+// not a component
+const API_KEY = "19539c8d4b6179bb63e28cb8395940c7";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;  
